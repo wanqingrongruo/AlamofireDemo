b72089bb31a9bda07b450d059a217e8aa60f1669
婉卿容若 <devzheng@qq.com>
2017-04-21 16:00:19 +0800

    备份

--- AlamofireBoxue/AlamofireBoxue/ViewController.swift
+++ AlamofireBoxue/AlamofireBoxue/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("https://httpbin.org/ip", method: .get).response { response in
+            print(response.request ?? "")
+            print(response.response ?? "")
+            print(response.data ?? "")
+            print(response.error ?? "")
+        }.responseString { (response) in
+            print("String ----------")
+            
+            switch response.result {
+            case .success(let str):
+                print("\(type(of: str))")
+            case .failure(let error):
+                print("\(error)")
+                
+            }
+        }
+        
+        
+        let params = ["post": "value", "arr": ["1","2"], "dic": ["key1": "value1", "key2": "value2"]] as [String : Any]
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
